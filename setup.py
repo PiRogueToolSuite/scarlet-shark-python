98178daea5530f70fa97a0258385c15a9de5bd99
U039b <git@0x39b.fr>
2023-05-10 13:04:52 +0200

    Add packaging for Debian

--- setup.py
+++ setup.py
@@ -18,21 +18,22 @@ install_requires = [
     'requests==2.29.0'
 ]
 
-setup(name='scarlet-shark-python',
-      version='1.0.3',
-      description='Scarlet Shark REST API Python client',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='U+039b',
-      author_email='esther@pts-project.org',
-      url='https://github.com/PiRogueToolSuite/scarlet-shark-python',
-      packages=find_packages(exclude=['*.tests', '*.tests.*', 'test*', 'tests']),
-      install_requires=install_requires,
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'License :: OSI Approved :: GNU Affero General Public License v3',
-          'Natural Language :: English',
-          'Topic :: Security',
-          'Topic :: Utilities',
-      ]
-      )
+setup(
+    name='scarlet-shark-python',
+    version='1.0.3',
+    description='Scarlet Shark REST API Python client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='U+039b',
+    author_email='esther@pts-project.org',
+    url='https://github.com/PiRogueToolSuite/scarlet-shark-python',
+    packages=find_packages(exclude=['*.tests', '*.tests.*', 'test*', 'tests']),
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Natural Language :: English',
+        'Topic :: Security',
+        'Topic :: Utilities',
+    ]
+)
